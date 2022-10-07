42de6c87623bb34e7837d53d5dd250d9ec19346a
carrebola <carrebola2223@gmail.com>
2022-10-07 16:41:21 +0200

    ok

---
+++ index.js
@@ -0,0 +1,79 @@
+const form = document.querySelector("form");
+		const validar = document.querySelector("#validar");
+		const enviar = document.querySelector("#enviar");
+
+		validar.addEventListener("click", (e) => {
+			console.log("validar");
+			e.preventDefault();
+			//console.log(form.user.value,form.pass.value);
+			//petición a la api
+			const peticionValidar = async () => {
+				user = document.querySelector("#user").value;
+				pass = 
+					(document.querySelector("#d1").value)+
+                    (document.querySelector("#d2").value)+
+                    (document.querySelector("#d3").value)+
+                    (document.querySelector("#d4").value)+
+                    (document.querySelector("#d5").value)+
+                    (document.querySelector("#d6").value)
+				;
+
+				console.log(pass);
+				const url = `http://localhost:8000/api/validar?user=${user}&pass=${pass}`;
+				var respuesta = await fetch(url);
+				respuesta = await respuesta.json();
+				var icono = "";
+				var titulo = "";
+				if (respuesta.error == false) {
+					icono = "success";
+					titulo = "¡Buen trabajo!";
+					document.querySelectorAll(".digito").forEach((elemento) => {
+						elemento.classList.add("bg-success");
+					});
+					document.querySelector("#enviar").classList.remove("disabled");
+				} else {
+					icono = "warning";
+					titulo = "¡Uf...!";
+				}
+				//alert(respuesta.mensaje)
+
+				swal({
+					title: titulo,
+					text: respuesta.mensaje,
+					icon: icono,
+				});
+				//pintaExamen(respuesta)
+			};
+			peticionValidar();
+		});
+		enviar.addEventListener("click", (e) => {
+			e.preventDefault();
+			//console.log(form.user.value,form.pass.value);
+			//petición a la api
+			const peticionEnviar = async () => {
+				user = document.querySelector("#user").value;
+				pass = document.querySelector("#pass").value;
+                console.log("user",user);
+				const url = `http://localhost:8000/api/enviar?user=${user}&pass=${pass}`;
+				var respuesta = await fetch(url);
+				respuesta = await respuesta.json();
+
+				var icono = "";
+				var titulo = "";
+				if (respuesta.error == false) {
+					icono = "success";
+					titulo = "¡Buen trabajo!";
+					console.log(respuesta.datos.modelo, respuesta.datos.preguntas);
+					localStorage.setItem("datos", JSON.stringify(respuesta.datos));
+				} else {
+					icono = "warning";
+					titulo = "¡Uf...!";
+				}
+				swal({
+					title: titulo,
+					text: respuesta.mensaje,
+					icon: icono,
+				});
+			};
+			peticionEnviar();
+		});
